5e1b3c60d5abfa138c87b1c2eef2254a881ad9b0
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-07-02 17:57:57 +0200

    socket io implementation

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     token: null,
     userId: null,
     authLoading: false,
-    error: null,
+    error: null
   };
 
   componentDidMount() {
@@ -41,7 +41,7 @@ class App extends Component {
     this.setAutoLogout(remainingMilliseconds);
   }
 
-  mobileNavHandler = (isOpen) => {
+  mobileNavHandler = isOpen => {
     this.setState({ showMobileNav: isOpen, showBackdrop: isOpen });
   };
 
@@ -62,14 +62,14 @@ class App extends Component {
     fetch('http://localhost:8080/auth/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         email: authData.email,
-        password: authData.password,
-      }),
+        password: authData.password
+      })
     })
-      .then((res) => {
+      .then(res => {
         if (res.status === 422) {
           throw new Error('Validation failed.');
         }
@@ -79,13 +79,13 @@ class App extends Component {
         }
         return res.json();
       })
-      .then((resData) => {
+      .then(resData => {
         console.log(resData);
         this.setState({
           isAuth: true,
           token: resData.token,
           authLoading: false,
-          userId: resData.userId,
+          userId: resData.userId
         });
         localStorage.setItem('token', resData.token);
         localStorage.setItem('userId', resData.userId);
@@ -96,12 +96,12 @@ class App extends Component {
         localStorage.setItem('expiryDate', expiryDate.toISOString());
         this.setAutoLogout(remainingMilliseconds);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         this.setState({
           isAuth: false,
           authLoading: false,
-          error: err,
+          error: err
         });
       });
   };
@@ -112,15 +112,15 @@ class App extends Component {
     fetch('http://localhost:8080/auth/signup', {
       method: 'PUT',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         email: authData.signupForm.email.value,
         password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value,
-      }),
+        name: authData.signupForm.name.value
+      })
     })
-      .then((res) => {
+      .then(res => {
         if (res.status === 422) {
           throw new Error(
             "Validation failed. Make sure the email address isn't used yet!"
@@ -132,22 +132,22 @@ class App extends Component {
         }
         return res.json();
       })
-      .then((resData) => {
+      .then(resData => {
         console.log(resData);
         this.setState({ isAuth: false, authLoading: false });
         this.props.history.replace('/');
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         this.setState({
           isAuth: false,
           authLoading: false,
-          error: err,
+          error: err
         });
       });
   };
 
-  setAutoLogout = (milliseconds) => {
+  setAutoLogout = milliseconds => {
     setTimeout(() => {
       this.logoutHandler();
     }, milliseconds);
@@ -161,9 +161,9 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route
-          path='/'
+          path="/"
           exact
-          render={(props) => (
+          render={props => (
             <LoginPage
               {...props}
               onLogin={this.loginHandler}
@@ -172,9 +172,9 @@ class App extends Component {
           )}
         />
         <Route
-          path='/signup'
+          path="/signup"
           exact
-          render={(props) => (
+          render={props => (
             <SignupPage
               {...props}
               onSignup={this.signupHandler}
@@ -182,22 +182,22 @@ class App extends Component {
             />
           )}
         />
-        <Redirect to='/' />
+        <Redirect to="/" />
       </Switch>
     );
     if (this.state.isAuth) {
       routes = (
         <Switch>
           <Route
-            path='/'
+            path="/"
             exact
-            render={(props) => (
+            render={props => (
               <FeedPage userId={this.state.userId} token={this.state.token} />
             )}
           />
           <Route
-            path='/:postId'
-            render={(props) => (
+            path="/:postId"
+            render={props => (
               <SinglePostPage
                 {...props}
                 userId={this.state.userId}
@@ -205,7 +205,7 @@ class App extends Component {
               />
             )}
           />
-          <Redirect to='/' />
+          <Redirect to="/" />
         </Switch>
       );
     }
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -39,7 +40,48 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    const socket = openSocket('http://localhost:8080');
+    socket.on('posts', (data) => {
+      if (data.action === 'create') {
+        this.addPost(data.post);
+      } else if (data.action === 'update') {
+        this.updatePost(data.post);
+      } else if (data.action === 'delete') {
+        this.loadPosts();
+      }
+    });
   }
+
+  addPost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
+      };
+    });
+  };
 
   loadPosts = (direction) => {
     if (direction) {
@@ -68,7 +110,10 @@ class Feed extends Component {
       .then((resData) => {
         this.setState({
           posts: resData.posts.map((post) => {
-            return { ...post, imagePath: post.imageUrl };
+            return {
+              ...post,
+              imagePath: post.imageUrl,
+            };
           }),
           totalPosts: resData.totalItems,
           postsLoading: false,
@@ -128,7 +173,6 @@ class Feed extends Component {
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
-
     let url = 'http://localhost:8080/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
@@ -159,17 +203,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt,
         };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else {
-            updatedPosts = prevState.posts.concat(post);
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
@@ -207,10 +241,11 @@ class Feed extends Component {
       })
       .then((resData) => {
         console.log(resData);
-        this.setState((prevState) => {
-          const updatedPosts = prevState.posts.filter((p) => p._id !== postId);
-          return { posts: updatedPosts, postsLoading: false };
-        });
+        this.loadPosts();
+        // this.setState(prevState => {
+        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId);
+        //   return { posts: updatedPosts, postsLoading: false };
+        // });
       })
       .catch((err) => {
         console.log(err);
